4e870877c1a358b413ea0756255edb43d14161b8
shwethaj1104 <shwethaj1104@gmail.com>
2022-10-28 19:10:18 +0530

    fetched data from api

---
+++ src/context.jsx
@@ -0,0 +1,24 @@
+// import React, { useContext, useState } from "react";
+
+
+
+// const AppContext = React.createContext()
+
+// export const AppProvider = ({ children }) => {
+//     const [modaldata, setmodalData] = useState({})
+//     const [isopenModal, setIsOpenModal] = useState(false)
+
+//     const openModal = (text) => {
+//         setIsOpenModal(true)
+//         setmodalData(text)
+//     }
+//     const closeModal = () => {
+//         setIsOpenModal(false)
+//     }
+
+//     return <AppContext.Provider value={{ isopenModal, openModal, closeModal, modaldata, setmodalData }} >{children}</AppContext.Provider>
+// }
+
+// export const useGlobalContext = () => {
+//     return useContext(AppContext)
+// }
